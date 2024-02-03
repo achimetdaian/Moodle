1397c36e31bdb9dd5083b4f2908881cdaae6f2a9
achimetdaian <achimetemanueldaian@gmail.com>
2024-02-03 01:51:05 -0800

    aeulfbaeubf laeubflaub liabefak

--- index.php
+++ index.php
@@ -28,9 +28,7 @@
  $context = context_system::instance();
 
  $PAGE->set_context($context);
- 
  $PAGE->set_url(new moodle_url('/local/greetings/index.php'));
- 
  $PAGE->set_pagelayout('standard');
  $PAGE->set_title($SITE->fullname);
  $PAGE->set_heading(get_string('pluginname', 'local_greetings'));
